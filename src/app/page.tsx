5a58e1e0e7fa9830d72b75c2f853dcdc2d682d78
jungminji <wjdalswl0215@gmail.com>
2024-09-27 01:08:59 +0900

    update : 홈에서 페이지 이동 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>롤 정보 페이지입니다</div>;
+  return (
+    <div>
+      <h2>리그 오브 레전드 정보 앱</h2>
+      <div className="flex flex-col gap-10">
+        <Link href={"/"}>홈</Link>
+        <Link href={"/champions"}>챔피언 목록</Link>
+        <Link href={"/items"}>아이템 목록</Link>
+        <Link href={"/rotation"}>챔피언 로테이션</Link>
+      </div>
+    </div>
+  );
 }
